61cd02880c6c5f81a2624031ccbed9870c83466a
Sandeep Thapar <69153672+fit-coder@users.noreply.github.com>
2020-08-07 01:16:59 +0530

    Represent graph using adjacency list

---
+++ graph/adjacency_list.cpp
@@ -0,0 +1,38 @@
+// Print adjacency list for a graph
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#define V 5
+
+void addEdge(vector<int> graph[], int u, int v)
+{
+    graph[u].emplace_back(v);
+}
+
+int main()
+{
+    // Array of vectors, every vector represents
+    // adjacency list of a vertex
+    vector<int> graph[V];
+
+    addEdge(graph, 0, 1);
+    addEdge(graph, 0, 4);
+    addEdge(graph, 1, 1);
+    addEdge(graph, 1, 2);
+    addEdge(graph, 1, 3);
+    addEdge(graph, 1, 4);
+    addEdge(graph, 3, 2);
+    addEdge(graph, 3, 4);
+
+    cout << "Adjacency List:" << endl;
+    for (int i=0; i < V; i++)
+    {
+        cout << i;
+        for (int j=0; j < graph[i].size(); j++)
+            cout << " -> " << graph[i][j];
+        cout << endl;
+    }
+    return 0;
+}
---
+++ graph/adjacency_list_weighted.cpp
@@ -0,0 +1,38 @@
+// Print adjacency list for a weighted graph
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#define V 5
+
+void addEdge(vector<pair<int,int>> graph[], int u, int v, int weight)
+{
+    graph[u].emplace_back(make_pair(v, weight));
+}
+
+int main()
+{
+    // Array of vectors, every vector represents
+    // adjacency list of a vertex
+    vector<pair<int,int>> graph[V];
+
+    addEdge(graph, 0, 1, 1);
+    addEdge(graph, 0, 4, 5);
+    addEdge(graph, 1, 1, 2);
+    addEdge(graph, 1, 2, 3);
+    addEdge(graph, 1, 3, 4);
+    addEdge(graph, 1, 4, 2);
+    addEdge(graph, 3, 2, 2);
+    addEdge(graph, 3, 4, 3);
+
+    cout << "Adjacency List:" << endl;
+    for (int i=0; i < V; i++)
+    {
+        cout << i;
+        for (int j=0; j < graph[i].size(); j++)
+            cout << " -> " << graph[i][j].first << "," << graph[i][j].second;
+        cout << endl;
+    }
+    return 0;
+}
