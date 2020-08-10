e199ba249a6a6222c82a83d950cd9eb720285026
Sandeep Thapar <69153672+fit-coder@users.noreply.github.com>
2020-08-11 00:41:51 +0530

    Incidence Matrix and Incidence List for directed unweighted graph

---
+++ graph/incidence_list.cpp
@@ -0,0 +1,41 @@
+// Print incidence list for a directed unweighted graph
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#define V 5
+#define E 8
+
+void addEdge(vector<pair<int,int>> graph[], int u, int v, int e)
+{
+    graph[u].emplace_back(make_pair(e,v));
+}
+
+int main()
+{
+    // array of vectors of pair of <edge,endvertex>
+    vector<pair<int,int>> graph[V];
+    
+    // graph, start, end, edge
+    addEdge(graph, 0, 1, 0);
+    addEdge(graph, 0, 4, 1);
+    addEdge(graph, 1, 4, 2);
+    addEdge(graph, 1, 3, 3);
+    addEdge(graph, 1, 1, 5);
+    addEdge(graph, 1, 2, 6);
+    addEdge(graph, 3, 4, 4);
+    addEdge(graph, 3, 2, 7);
+
+    cout << "Incidence list:" << endl;
+    for (int i=0; i < V; i++)
+    {
+        cout << i;
+        for (int j=0; j < graph[i].size(); j++)
+            cout << " --> " << graph[i][j].first << "," << graph[i][j].second;
+        cout << endl;
+    }
+    return 0;
+}
+
---
+++ graph/incidence_matrix.cpp
@@ -0,0 +1,40 @@
+// Draw incidence matrix for a directed unweighted graph
+
+#include <iostream>
+using namespace std;
+
+#define V 5
+#define E 8
+
+void addEdge(int graph[][E], int u, int v, int e)
+{
+    graph[u][e] = 1;
+    if (u != v)
+       graph[v][e] = -1;
+}
+
+int main()
+{
+    // 2d array of V*E
+    int graph[V][E] = {0};
+
+    // graph, start, end, edge
+    addEdge(graph, 0, 1, 0);
+    addEdge(graph, 0, 4, 1);
+    addEdge(graph, 1, 4, 2);
+    addEdge(graph, 1, 3, 3);
+    addEdge(graph, 1, 1, 5);
+    addEdge(graph, 1, 2, 6);
+    addEdge(graph, 3, 4, 4);
+    addEdge(graph, 3, 2, 7);
+
+    cout << "Incidence matrix:" << endl;
+    for (int i=0; i < V; i++)
+    {
+        for (int j=0; j < E; j++)
+            cout << graph[i][j] << "   ";
+        cout << endl;
+    }
+    return 0;
+}
+
