a830f4c72fd54c491b29d876f67484731ab0b1d6
Sandeep Thapar <69153672+fit-coder@users.noreply.github.com>
2020-09-13 18:41:41 +0530

    Topological sort

---
+++ graph/topological_sort_dfs.cpp
@@ -0,0 +1,44 @@
+// Topological sort using modified DFS for a directed acyclic graph
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void addEdge(vector<int> graph[], int u, int v) {
+    graph[u].emplace_back(v);
+    graph[v].emplace_back(u);
+}
+
+void topological_sort(vector<int> graph[], vector<bool>& visited,
+                      vector<int>& result, int node)
+{
+    visited[node] = true;
+    for (auto i = graph[node].begin(); i != graph[node].end(); i++) {
+        if (!visited[*i])
+           topological_sort(graph, visited, result, *i);
+    }
+    result.emplace_back(node);
+}
+
+int main() {
+    vector<int> graph[5];
+    vector<bool> visited(5, false);
+
+    addEdge(graph, 0, 1);
+    addEdge(graph, 0, 2);
+    addEdge(graph, 1, 2);
+    addEdge(graph, 1, 3);
+    addEdge(graph, 2, 3);
+    addEdge(graph, 2, 4);
+    
+    vector<int> result ;
+    cout << "Topological sort using DFS: " << endl;
+    topological_sort(graph, visited, result, 0);
+    for (int i=result.size()-1; i >= 0; i--)
+        cout << result[i] << " ";
+    cout << endl;
+
+    return 0;
+}
+
