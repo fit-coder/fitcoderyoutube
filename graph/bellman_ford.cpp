85841957e0ba5557484e96251da32ba5bcb76522
Sandeep Thapar <69153672+fit-coder@users.noreply.github.com>
2020-10-15 18:54:26 +0530

    Bellman ford's algorithm

---
+++ graph/bellman_ford.cpp
@@ -0,0 +1,70 @@
+// Bellman ford's algorithm for shortest path distance
+
+#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+bool bellman_ford(map<pair<int,int>,int>& edges,
+                  vector<int>& dist,
+                  int V)
+{
+    for (int i=1; i < V; i++)
+    {
+        for (auto e : edges)
+        {
+            int u = e.first.first;
+            int v = e.first.second;
+            int weight = e.second;
+            if (dist[v] > dist[u] + weight)
+               dist[v] = dist[u] + weight;
+        }
+    }
+    for (auto e : edges)
+    {
+        int u = e.first.first;
+        int v = e.first.second;
+        int weight = e.second;
+        if (dist[v] > dist[u] + weight)
+            return false;
+    }
+    return true;
+}
+
+void addEdge(map<pair<int,int>,int>& edges,
+             int u, int v, int w)
+{
+    edges[make_pair(u,v)] = w;
+}
+
+int main()
+{
+    int V = 5;
+    // pair of vertex, weight
+    map<pair<int,int>,int> edges;
+
+    addEdge(edges, 0, 1, 4);
+    addEdge(edges, 0, 2, 2);
+    addEdge(edges, 1, 3, 2);
+    addEdge(edges, 1, 4, 3);
+    addEdge(edges, 1, 2, 3);
+    addEdge(edges, 2, 1, 1);
+    addEdge(edges, 2, 4, 5);
+    addEdge(edges, 2, 3, 4);
+    addEdge(edges, 4, 3, -5);
+
+    vector<int> dist(V, INT_MAX);
+    dist[0] = 0;
+    bool res = bellman_ford(edges, dist, V);
+    if (!res)
+        cout << "Negative-weight cycle exists" << endl;
+    else
+    {
+        cout << "Shortest path distance from start vertex (0): " << endl;
+        for (int i=0; i < V; i++)
+            cout << "0-" << i << " : " << dist[i] << endl;
+    }
+    return 0;
+}
+
