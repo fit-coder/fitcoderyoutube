146582c2d27deaef3561d3781473ac76d01f45fa
Sandeep Thapar <69153672+fit-coder@users.noreply.github.com>
2020-11-07 22:49:30 +0530

    Ford fulkerson algorithm

---
+++ graph/ford_fulkerson.cpp
@@ -0,0 +1,73 @@
+// Ford fulkerson algorithm for maximum flow
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int bfs(int source, int sink, vector<int>& parent, vector<vector<int>>& residualGraph) {
+    fill(parent.begin(), parent.end(), -1);
+    int V = residualGraph.size();
+    parent[source] = -2;
+    queue<pair<int, int>> q;
+    q.push({source, INT_MAX});
+
+    while (!q.empty()) {
+        int u = q.front().first;
+        int capacity = q.front().second;
+        q.pop();
+        for (int av=0; av < V; av++) {
+            if (u != av && parent[av] == -1 && residualGraph[u][av] != 0) {
+                parent[av] = u;
+                int min_cap = min(capacity, residualGraph[u][av]);
+                if (av == sink)
+                    return min_cap;
+                q.push({av, min_cap});
+            }
+        }
+    }
+    return 0;
+}
+
+int ford_fulkerson(vector<vector<int>>& graph, int source, int sink) {
+    vector<int> parent(graph.size(), -1);
+    vector<vector<int>> residualGraph = graph;
+    int min_cap = 0, max_flow = 0;
+    while (min_cap = bfs(source, sink, parent, residualGraph)) {
+        max_flow += min_cap;
+        int u = sink;
+        while (u != source) {
+            int v = parent[u];
+            residualGraph[u][v] += min_cap;
+            residualGraph[v][u] -= min_cap;
+            u = v;
+        }
+    }
+    return max_flow;
+}
+
+void addEdge(vector<vector<int>>& graph,
+             int u, int v, int w)
+{
+    graph[u][v] = w;
+}
+
+int main()
+{
+    int V = 6;
+    vector<vector<int>> graph(V, vector<int> (V, 0));
+
+    addEdge(graph, 0, 1, 4);
+    addEdge(graph, 0, 3, 3);
+    addEdge(graph, 1, 2, 4);
+    addEdge(graph, 2, 3, 3);
+    addEdge(graph, 2, 5, 2);
+    addEdge(graph, 3, 4, 6);
+    addEdge(graph, 4, 5, 6);
+    
+    cout << "Maximum Flow: " << ford_fulkerson(graph, 0, 5) << endl;
+    return 0;
+}
+
