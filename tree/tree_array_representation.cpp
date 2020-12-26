b6d2cb1481f19fd551bda41aa20708c0278b78ed
Sandeep Thapar <69153672+fit-coder@users.noreply.github.com>
2020-12-26 14:06:59 +0530

    Add video explaination link

--- tree/tree_array_representation.cpp
+++ tree/tree_array_representation.cpp
@@ -1,37 +1,43 @@
+/*
+** For video explanation, please refer to my Youtube channel in the link below:
+** https://www.youtube.com/channel/UCX6rLou1VXXPVsORMVkUryg/videos
+** Please subscribe if you like the content
+*/
+
 #include <iostream>
 using namespace std;
 
 void set_left(char tree[], char val, int parent)
 {
-	tree[2*parent + 1] = val;
+    tree[2*parent + 1] = val;
 }
 
 void set_right(char tree[], char val, int parent)
 {
-	tree[2*parent + 2] = val;
+    tree[2*parent + 2] = val;
 }
 
 int main()
 {
-	int N = 15;
-	char tree[N] = {0};
-	
-	tree[0] = 'M';
-	set_left(tree, 'P', 0);
-	set_right(tree, 'Q', 0);
-	set_left(tree, 'A', 1);
-	set_right(tree, 'D', 1);
-	set_left(tree, 'B', 2);
-	set_right(tree, 'C', 2);
-	set_right(tree, 'W', 6);
+    int N = 15;
+    char tree[N] = {0};
+    
+    tree[0] = 'M';
+    set_left(tree, 'P', 0);
+    set_right(tree, 'Q', 0);
+    set_left(tree, 'A', 1);
+    set_right(tree, 'D', 1);
+    set_left(tree, 'B', 2);
+    set_right(tree, 'C', 2);
+    set_right(tree, 'W', 6);
 
-	for (int i=0; i < N; i++)
-	{
-		if (tree[i] == '\0')
+    for (int i=0; i < N; i++)
+    {
+        if (tree[i] == '\0')
             cout << "-" << " ";
-		else
-			cout << tree[i] << " ";
-	}
-	return 0;
+        else
+            cout << tree[i] << " ";
+    }
+    return 0;
 }
 
--- tree/tree_linked_representation.cpp
+++ tree/tree_linked_representation.cpp
@@ -1,30 +1,35 @@
+/*
+** For video explanation, please refer to my Youtube channel in the link below:
+** https://www.youtube.com/channel/UCX6rLou1VXXPVsORMVkUryg/videos
+** Please subscribe if you like the content
+*/
+
 #include <iostream>
 using namespace std;
 
 struct Node
 {
-	char val;
-	Node* left;
-	Node* right;
+    char val;
+    Node* left;
+    Node* right;
 
-	Node(char data)
-	{
-		val = data;
-		left = NULL;
-		right = NULL;
-	}
+    Node(char data)
+    {
+        val = data;
+        left = NULL;
+        right = NULL;
+    }
 };
 
 int main()
 {
-	Node* root = new Node('M');
-	root->left = new Node('P');
-	root->right = new Node('Q');
-	root->left->left = new Node('A');
-	root->left->right = new Node('D');
-	root->right->left = new Node('B');
-	root->right->right = new Node('C');
-	root->right->right->right = new Node('W');
-	return 0;
+    Node* root = new Node('M');
+    root->left = new Node('P');
+    root->right = new Node('Q');
+    root->left->left = new Node('A');
+    root->left->right = new Node('D');
+    root->right->left = new Node('B');
+    root->right->right = new Node('C');
+    root->right->right->right = new Node('W');
+    return 0;
 }
-
