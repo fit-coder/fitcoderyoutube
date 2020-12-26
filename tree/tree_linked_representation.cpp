ad33aaa861f5e1821256c52ea4b23f689c4b4ec1
Sandeep Thapar <69153672+fit-coder@users.noreply.github.com>
2020-12-26 14:04:44 +0530

    Tree representation - array and linked

---
+++ tree/tree_linked_representation.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+	char val;
+	Node* left;
+	Node* right;
+
+	Node(char data)
+	{
+		val = data;
+		left = NULL;
+		right = NULL;
+	}
+};
+
+int main()
+{
+	Node* root = new Node('M');
+	root->left = new Node('P');
+	root->right = new Node('Q');
+	root->left->left = new Node('A');
+	root->left->right = new Node('D');
+	root->right->left = new Node('B');
+	root->right->right = new Node('C');
+	root->right->right->right = new Node('W');
+	return 0;
+}
+
