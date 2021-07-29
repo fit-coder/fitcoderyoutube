81f2d2dd106706e78cbcab1dce5602d8897463ec
Sandeep Thapar <69153672+fit-coder@users.noreply.github.com>
2021-07-29 23:18:10 +0530

    Fix issue due to adding value to INT_MAX

--- graph/bellman_ford.cpp
+++ graph/bellman_ford.cpp
@@ -16,18 +16,18 @@
 
 using namespace std;
 
-bool bellman_ford(map<pair<int,int>,int>& edges,
+bool bellman_ford(int start, map<pair<int,int>,int>& edges,
                   vector<int>& dist,
                   int V)
 {
-    for (int i=1; i < V; i++)
+    for (int i=0; i < V; i++)
     {
         for (auto e : edges)
         {
             int u = e.first.first;
             int v = e.first.second;
             int weight = e.second;
-            if (dist[v] > dist[u] + weight)
+            if (dist[u] != INT_MAX && (dist[v] > dist[u] + weight))
                dist[v] = dist[u] + weight;
         }
     }
@@ -36,7 +36,7 @@ bool bellman_ford(map<pair<int,int>,int>& edges,
         int u = e.first.first;
         int v = e.first.second;
         int weight = e.second;
-        if (dist[v] > dist[u] + weight)
+        if (dist[u] != INT_MAX && (dist[v] > dist[u] + weight))
             return false;
     }
     return true;
@@ -65,15 +65,16 @@ int main()
     addEdge(edges, 4, 3, -5);
 
     vector<int> dist(V, INT_MAX);
-    dist[0] = 0;
-    bool res = bellman_ford(edges, dist, V);
+    int start = 0;
+    dist[start] = 0;
+    bool res = bellman_ford(start, edges, dist, V);
     if (!res)
         cout << "Negative-weight cycle exists" << endl;
     else
     {
-        cout << "Shortest path distance from start vertex (0): " << endl;
+        cout << "Shortest path distance from start vertex (" << start << ")" << endl;
         for (int i=0; i < V; i++)
-            cout << "0-" << i << " : " << dist[i] << endl;
+            cout << start << "-" << i << " : " << dist[i] << endl;
     }
     return 0;
 }
